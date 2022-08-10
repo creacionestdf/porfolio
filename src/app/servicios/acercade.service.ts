ef634bc8d292ab50817b351e11f7263221a97ce1
creacionestdf <creacionestdf@outlook.com.ar>
2022-08-10 20:13:00 -0300

    actualizacion variada

--- src/app/servicios/acercade.service.ts
+++ src/app/servicios/acercade.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ace } from "../componentes/acerca-de/faceAcercade";
 import { environment } from "src/environments/environment.prod";
-
+/*
 const httpOptions : any    = {
   headers: new HttpHeaders({
     //'Content-Type':  'application/json',
@@ -11,7 +11,7 @@ const httpOptions : any    = {
     'Access-Control-Allow-Methods': 'GET',
     'Access-Control-Allow-Origin': '*'
   })};
-
+*/
 @Injectable({
   providedIn: 'root'
 })

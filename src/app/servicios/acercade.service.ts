e6518bf761062e4f64016de77ae9535c4f91c59a
creacionestdf <creacionestdf@outlook.com.ar>
2022-08-10 20:09:54 -0300

    actualizacion variada

--- src/app/servicios/acercade.service.ts
+++ src/app/servicios/acercade.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Ace } from "../componentes/acerca-de/faceAcercade";
+import { environment } from "src/environments/environment.prod";
 
 const httpOptions : any    = {
   headers: new HttpHeaders({
@@ -17,21 +18,21 @@ const httpOptions : any    = {
 export class AcercadeService {
 
   //URL s
-  private url:String = "https://porfolioweb-backend.herokuapp.com/acercade/traer";
-  private UrlSave = 'porfolioweb-backend.herokuapp.com/acercade/actualizar';
+  private url:String = environment.apiBaseURL;
+  private UrlSave = environment.apiBaseURL + '/acercade/actualizar';
   
 
   constructor(private http: HttpClient) {}
 
   //OBTENEMOS todos los registros...
   getAll(): Observable<Ace[]> {
-    return this.http.get<Ace[]>(`${this.url}`);
+    return this.http.get<Ace[]>(this.url+"/acercade/traer");
   }
 
   get(){
     return this.http.get(`${this.url}`);
   }
-  
+
   //GUARDAMOS cambios realizados
   actualizar(obj:Ace): Observable<Object> {
     return this.http.put<Ace>(`${this.UrlSave}/${obj.id}`,obj);

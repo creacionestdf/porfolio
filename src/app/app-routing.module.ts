fd0d49ce4836ee747c3fba0506bfbe2fae6e95a3
creacionestdf <creacionestdf@outlook.com.ar>
2022-08-21 15:50:01 -0300

    modif

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { RegistroComponent } from "./componentes/registro/registro.component";
 
 const routes: Routes = [
    
-  { path: 'home', component: HomeComponent},
+  { path: '', component: HomeComponent},
   { path: 'login', component: RegistroComponent },
   { path: 'about', component: AboutComponent }
   
